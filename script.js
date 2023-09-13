69008c17940871f955e9b46212ae8dfe8e06cccd
Stjepan1999 <stjepanfranic99@gmail.com>
2023-09-13 18:03:28 +0200

    Add functions to buttons and modified html

--- script.js
+++ script.js
@@ -1,78 +1,108 @@
-let choices = ["rock", "paper", "scissors"]
+
 let playerWins = 0;
 let computerWins = 0;
+let currentPlayerChoice = null; // Variables used for button animation
+let currentComputerChoice = null
 
 const buttons = document.querySelectorAll("button");
+
 buttons.forEach((button) => {
-	button.addEventListener("click", () => playRound(button.id, getComputerChoice()))
+	button.addEventListener("click", () => {
+  	let playerSelection = button.id
+    let computerSelection = getComputerChoice()
+
+    
+    if (currentPlayerChoice) {
+    	const previousPlayerChoice = document.getElementById(currentPlayerChoice);
+      previousPlayerChoice.classList.remove("current-choice");
+      }
+    
+    if (currentComputerChoice){
+      const previousComputerChoice = document.getElementById(`computer-${currentComputerChoice}`);
+      previousComputerChoice.classList.remove("current-computer-choice");
+    }
+    
+  	
+    currentComputerChoice = computerSelection
+    const computerChoiceElement = document.getElementById(`computer-${computerSelection}`)
+    computerChoiceElement.classList.add("current-computer-choice");
+	
+    button.classList.add("current-choice");
+    currentPlayerChoice = playerSelection;
+    playRound(playerSelection, computerSelection);
+  	})
 })
 
-const resultDiv = document.querySelector(".result");
-const result = document.createElement("div");
-resultDiv.appendChild(result);
-const scoreDiv = document.querySelector(".score");
-const playerScore = document.createElement("div")
-playerScore.textContent = "testing"
-scoreDiv.appendChild(playerScore)
-  
-function showResult(computerSelection, message) {
+
+const playerScoreDiv = document.querySelector(".player-score")
+const computerScoreDiv = document.querySelector(".computer-score")
+const winnerDiv = document.querySelector("#winner")
+
+function showResult() {
 	if (playerWins < 5 && computerWins < 5) {
-  result.textContent = `Computer choice is ${computerSelection}. ${message}. Result is: Player : ${playerWins} Computer: ${computerWins}`;
-  playerScore.textContent = playerWins;
-  scoreDiv.appendChild(playerScore);
-  } else {if (playerWins > computerWins) {
-  	result.textContent = `You won!`;
+    playerScoreDiv.textContent = `SCORE: ${playerWins}`;
+    computerScoreDiv.textContent = `SCORE: ${computerWins}`;
+  
+  } else if (playerWins === 5 || computerWins === 5){ 
+  	playerScoreDiv.textContent = `SCORE: ${playerWins}`;
+    computerScoreDiv.textContent = `SCORE: ${computerWins}`;
+  {if (playerWins > computerWins) {
+  	winnerDiv.textContent = `You won the game!`;
+    winnerDiv.style.color =  "#FAC650";
     playerWins = 0;
-    computerWins = 0;
-  } else {
-  	result.textContent = `Game over, you lost!`;
-    playerWins = 0;
-    computerWins = 0;
-  }}
- 
-  resultDiv.appendChild(result);
+  	computerWins = 0;
+    } else {
+      winnerDiv.textContent = `Game over, you lost!`;
+      winnerDiv.style.color =  "red";
+      playerWins = 0;
+ 		  computerWins = 0;
+    }}}
+  
+	
 }
 
 
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === "rock" && computerSelection === "paper") {
   	computerWins++;
-    let message = "You Lose! Paper beats Rock"
-    showResult(computerSelection, message);
+    winnerDiv.textContent = "You lost";
+    winnerDiv.style.color =  "red";
   }
   else if (playerSelection === "rock" && computerSelection === "scissors") {
   	playerWins++;
-    let message = "You Win! Rock beats Scissors"
-    showResult(computerSelection, message);
+    winnerDiv.textContent = "You won";
+    winnerDiv.style.color =  "#FAC650";
   }
   else if (playerSelection === "scissors" && computerSelection === "paper") {
   	playerWins++;
-    let message = "You Win! Scissors beats Paper"
-    showResult(computerSelection, message);
+    winnerDiv.textContent = "You won";
+    winnerDiv.style.color =  "#FAC650";
   }
   else if (playerSelection === "scissors" && computerSelection === "rock") {
   	computerWins++;
-    let message = "You Lose! Rock beats Scissors"
-  	showResult(computerSelection, message);
+    winnerDiv.textContent = "You lost";
+    winnerDiv.style.color =  "red";
   }
   else if (playerSelection === "paper" && computerSelection === "rock") {
   	playerWins++;
-    let message = "You Win! Paper beats Rock"
-  	showResult(computerSelection, message);
+    winnerDiv.textContent = "You won";
+    winnerDiv.style.color =  "#FAC650";
   }
   else if (playerSelection === "paper" && computerSelection === "scissors") {
   	computerWins++;
-    let message = "You Lose! Scissors beats Paper"
-  	showResult(computerSelection, message);
+    winnerDiv.textContent = "You lost";
+    winnerDiv.style.color =  "red";
   }
   else if (playerSelection === computerSelection) {
-  	let message = "It is tie";
-    showResult(computerSelection, message);
+  	winnerDiv.textContent = "It's tie";
+    winnerDiv.style.color =  "#222831";
   }
+  showResult();
 }
 
 function getComputerChoice () {
-	random = Math.floor(Math.random() * choices.length);
-	return choices[random];
+	let choices = ["rock", "paper", "scissors"]
+	randomIndex = Math.floor(Math.random() * choices.length);
+	return choices[randomIndex];
 }
 
